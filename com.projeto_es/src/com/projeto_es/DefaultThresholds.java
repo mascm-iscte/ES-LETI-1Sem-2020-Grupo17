92a74c0a9b6f05e033fa2c31053462f115657c45
pmcfg-iscte <pmcfg@iscte-iul.pt>
2020-11-16 17:13:04 +0000

    Default Thresholds

---
+++ com.projeto_es/src/com/projeto_es/DefaultThresholds.java
@@ -0,0 +1,29 @@
+package com.projeto_es;
+
+public class DefaultThresholds {
+	
+	public DefaultThresholds () {
+		
+	}
+	
+	
+	public boolean isLongMethod(int LOC, int CYCLO) {
+		return (LOC>80 && CYCLO>10);
+	}
+	
+	
+	public boolean isFeatureEnvy(int ATFD, int LAA, int NOFA) {
+		return ((ATFD>4 && LAA<0.428571) || (ATFD>4 && LAA>0.428571 && NOFA>6));
+		
+	}
+	
+	public boolean isGodClass(int WMCNAMM, int NOMNAMM, int RFC) {
+		return (WMCNAMM>47 || (WMCNAMM <= 47 && NOMNAMM >9 && RFC > 34));
+	}
+	
+	public boolean isDataClass(int NOAM, int WMCNAMM, int NIM) {
+		return (NOAM > 2 && WMCNAMM < 21 && NIM <= 30);
+	}
+	
+
+}
