6534980a8c337a162564b7bb18c617a42b523fb3
mascm-iscte <mascm@iscte-iul.pt>
2020-11-18 07:44:01 +0000

    Added GUI JTable implementation.

--- com.projeto_es/src/com/projeto_es/ExcelMethods.java
+++ com.projeto_es/src/com/projeto_es/ExcelMethods.java
@@ -1,11 +1,164 @@
 package com.projeto_es;
 
-public class ExcelMethods {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.LinkedList;
 
-	public ExcelMethods() {
-		System.out.println("Teste");
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbookFactory;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
+
+public class ExcelMethods {
+	
+	private Workbook wb = null;
+	private Sheet sh = null;
+	
+	/**
+	 * 
+	 * @param Workbook
+	 * @param Sheet
+	 */
+	public ExcelMethods(Workbook wb, Sheet sh) {
+		this.wb = wb;
+		this.sh = sh;
 	}
 	
+	public void setWB(Workbook wb) {
+		this.wb = wb;
+	}
+	
+	public void setSH(Sheet sh) {
+		this.sh = sh;
+	}	
+	
+	/**
+	 * @param filedirectory
+	 * @return workbook or null if problem getting file occurs
+	 */
+	public Workbook getWorkbook(String fileDir) {
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(fileDir);
+			wb = XSSFWorkbookFactory.create(fis);
+			fis.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EncryptedDocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return wb;
+	}
+	
+	/**
+	 * @param sheetName
+	 * @return return sheet by name
+	 */
+	public Sheet getSheet(String sheetName) {
+		return wb.getSheet(sheetName);
+	}
+	/**
+	 * @return number of rows in current sheet
+	 */
+	public int getRows() {
+		return sh.getLastRowNum() + 1;
+	}
+	/**
+	 * @return number of columns in current sheet
+	 */
+	public int getCols() {
+		return sh.getRow(0).getLastCellNum();
+	}
+	/**
+	 * 
+	 * @param row
+	 * @param col
+	 * @return cell contents as a string
+	 */
+	public String getCellContentStr(int row, int col) {
+		if(col == -1) throw new IllegalArgumentException("Error col not found");
+		FormulaEvaluator objFormulaEvaluator = wb.getCreationHelper().createFormulaEvaluator();
+		DataFormatter objDefaultFormat = new DataFormatter();
+		Cell cell =	sh.getRow(row).getCell(col);
+		objFormulaEvaluator.evaluate(cell);
+		return objDefaultFormat.formatCellValue(cell,objFormulaEvaluator);
+	}
+	
+	/**
+	 * @param name
+	 * @return number of the column with that name ( -1 means not found )
+	 */
+	
+	public int findColByName(String name) {
+		for (int i  = 0; i < getCols(); i++ ) {
+			if(name.equals(getCellContentStr(0, i))){
+				return i;
+			}
+		}
+		return -1;
+	}
+	/**
+	 * 
+	 * @param name
+	 * @return LinkedList<String> with values of the selected column
+	 */
+	public LinkedList<String> getFullColByName(String name) {
+		LinkedList<String> colValues = new LinkedList<String>();
+		int rows = getRows();
+		for( int i = 0; i < rows; i++ )
+			colValues.add(getCellContentStr(i, findColByName(name)));
+		return colValues;		
+	}
+	
+	/**
+	 * @param col
+	 * @return a LinkedList<String> of the selected column
+	 */
+	public LinkedList<String> getFullCol(int col) {
+		LinkedList<String> colValues = new LinkedList<String>();
+		int rows = getRows();
+		for( int i = 0; i < rows; i++ )
+			colValues.add(getCellContentStr(i, col));
+		return colValues;		
+	}
+	
+	/**
+	 * @param col_1
+	 * @param col_2
+	 * @return number of differences between 2 columns
+	 */
+	public int numberOfDif2ColsByName(String name_1, String name_2) {
+		int sumDiferences = 0;
+		int index = 0;
+		int col_1 = findColByName(name_1);
+		int col_2 = findColByName(name_2);
+		LinkedList<String> colValues_1 = new LinkedList<String>();
+		LinkedList<String> colValues_2 = new LinkedList<String>();
+		colValues_1.addAll(getFullCol(col_1));
+		colValues_2.addAll(getFullCol(col_2));
+		colValues_1.removeFirst();
+		colValues_2.removeFirst();
+		for (String string : colValues_1) {
+			if(!string.equals(colValues_2.get(index)))
+				sumDiferences++;
+			index++;
+		}	
+		return sumDiferences;
+	}
+	
+	
+	
+	//testes
 	public static void main(String[] args) {
 		
 	}
--- com.projeto_es/src/com/projeto_es/WindowGUI.java
+++ com.projeto_es/src/com/projeto_es/WindowGUI.java
@@ -7,18 +7,62 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Vector;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.graphbuilder.struc.LinkedList;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Rectangle;
+import java.awt.Button;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class WindowGUI {
 
 	protected static final Component parent = null;
-	private JFrame frame;
-	private JTable table;
-
+	private JFrame frmExcelSearch;
+	Vector headers = new Vector();
+	Vector data = new Vector();
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_3;
+	JTable table = new JTable();
+	String file_path;
+	String file_name;
+	ExcelMethods method;
+	DefaultTableModel model;
+	Workbook book = null;
+	Sheet sheet = null;
 	/**
 	 * Launch the application.
 	 */
@@ -27,7 +71,7 @@ public class WindowGUI {
 			public void run() {
 				try {
 					WindowGUI window = new WindowGUI();
-					window.frame.setVisible(true);
+					window.frmExcelSearch.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -41,34 +85,151 @@ public class WindowGUI {
 	public WindowGUI() {
 		initialize();
 	}
+	
+
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setLayout(null);
+		frmExcelSearch = new JFrame();
+		frmExcelSearch.setSize(1200, 800);
+		frmExcelSearch.getContentPane().setBounds(new Rectangle(1000, 1000, 1200, 800));
+		frmExcelSearch.getContentPane().setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		frmExcelSearch.setForeground(Color.BLUE);
+		frmExcelSearch.setFont(new Font("Calibri Light", Font.PLAIN, 14));
+		frmExcelSearch.setTitle("Excel Search");
 		
-		table = new JTable();
-		table.setBounds(116, 48, 661, 334);
-		frame.getContentPane().add(table);
 		
-		JButton btnNewButton = new JButton("Procurar");
+		
+		
+		
+		
+		
+		JButton btnNewButton = new JButton("Abrir");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				JFileChooser chooser = new JFileChooser();     
-				FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF Images", "jpg", "gif");     
-				chooser.setFileFilter(filter);     
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("Excel Spreadsheet", "xlsx"); 
+				chooser.setFileFilter(filter);
+				//Workbook book;
+				//Sheet sheet;
 				int returnVal = chooser.showOpenDialog(parent);     
-				if(returnVal == JFileChooser.APPROVE_OPTION) {        
-					System.out.println("You chose to open this file: " +chooser.getSelectedFile().getName());
-					System.out.println("Your directory is: " +chooser.getCurrentDirectory());
+				if(returnVal == JFileChooser.APPROVE_OPTION) {  
+					try {
+						file_path = chooser.getCurrentDirectory().toString();
+						file_name = chooser.getSelectedFile().getName();
+						method = new ExcelMethods(book, sheet);
+						book = method.getWorkbook(file_path+"\\"+file_name);
+						method.setWB(book);
+						method.setSH(book.getSheet("long-method"));
+						for (int i = 0; i < method.getCols(); i++) {
+							String headercol = method.getCellContentStr(0, i);
+							headers.add(headercol);
+						}
+						data.clear();
+						for (int i = 0; i < method.getRows(); i++) {
+						Vector d = new Vector();
+							for (int j = 0; j < method.getCols(); j++) {
+								String auxx = method.getCellContentStr(i, j);
+								d.add(auxx);
+							}	
+							d.add("\n");
+							data.add(d);
+						
+						}
+
+						}
+					
+						
+						catch (Exception e1) {
+						e1.printStackTrace();
+						}
+					
+					model = new DefaultTableModel(data,headers);
+					table.setModel(model);
+					table.setAutoCreateRowSorter(true);
+					model = new DefaultTableModel(data, headers);
+					table.setModel(model);
+					System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
+					System.out.println(("File directory: " + chooser.getCurrentDirectory()));
 					}
-				
 			}
 		});
-		btnNewButton.setBounds(659, 409, 89, 23);
-		frame.getContentPane().add(btnNewButton);
-	}
-}
+		
+		btnNewButton_1 = new JButton("Save");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+				boolean changesDetected = false;
+			    CreationHelper createhelper = book.getCreationHelper();
+			    sheet = book.getSheet("long-method");
+			    Row row = null;
+			    Cell cell = null;
+			    for (int i=0;i<model.getRowCount();i++) {
+			        row = sheet.getRow(i);
+			        for (int j=0;j<model.getColumnCount();j++) {
+			        	cell = row.getCell(j);
+			            cell.setCellValue((String) model.getValueAt(i, j));
+			        }
+			        
+			    }
+			    System.out.println("File saved");
+			    
+			    FileOutputStream out = new FileOutputStream("D:\\workbook.xls");
+			    book.write(out);
+			    out.close();
+			}  catch (IOException ex) {
+			    
+			}
+			}});
+			
+	
+			
+		    
+			   
+		
+		btnNewButton_2 = new JButton("Thresholds");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+
+		
+		btnNewButton_3 = new JButton("Procurar");
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+		GroupLayout groupLayout = new GroupLayout(frmExcelSearch.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(20)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnNewButton_2)
+					.addGap(18)
+					.addComponent(btnNewButton_1)
+					.addPreferredGap(ComponentPlacement.RELATED, 792, Short.MAX_VALUE)
+					.addComponent(btnNewButton_3)
+					.addGap(22))
+				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 1184, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 709, Short.MAX_VALUE)
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton_1)
+						.addComponent(btnNewButton)
+						.addComponent(btnNewButton_2)
+						.addComponent(btnNewButton_3))
+					.addContainerGap())
+		);
+		frmExcelSearch.getContentPane().setLayout(groupLayout);
+	}}
+		
+	
+		
