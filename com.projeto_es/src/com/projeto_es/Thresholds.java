8e36a2a2ed1700811bd90ba2ee2eecbd6d725e23
pmcfg-iscte <pmcfg@iscte-iul.pt>
2020-12-11 00:06:40 +0000

    Removi a métrica do NOFA. Miguel apaga os "NOFA_metric" na WindowGUI

--- com.projeto_es/src/com/projeto_es/Thresholds.java
+++ com.projeto_es/src/com/projeto_es/Thresholds.java
@@ -5,7 +5,6 @@ public class Thresholds {
 	private int CYCLO_metric;
 	private int ATFD_metric;
 	private int LAA_metric;
-	private int NOFA_metric;
 	
 	public Thresholds(){
 	}
@@ -42,8 +41,8 @@ public class Thresholds {
 	}
 	
 	
-	public boolean isFeatureEnvy(int ATFD, double LAA, int NOFA, int ATFD_metric, double LAA_metric, int NOFA_metric) {
-		return ((ATFD>ATFD_metric && LAA<LAA_metric) || (ATFD>ATFD_metric && LAA>LAA_metric && NOFA>NOFA_metric));
+	public boolean isFeatureEnvy(int ATFD, double LAA, int ATFD_metric, double LAA_metric) {
+		return ((ATFD>ATFD_metric && LAA<LAA_metric));
 		
 	}
 	public boolean isGodClass (int WMCNAMM, int WMCNAMM_metric, int NOMNAMM, int NOMNAMM_metric, int RFC, int RFC_metric){
